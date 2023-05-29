2dbc29bc46450c9f5ce47b685ad7bd9aa4f2bdf9
Crls <cj.cttn@gmail.com>
2023-05-29 20:30:00 -0300

    Changes in moon images, getMoonLapseIcon and app icon

--- app/src/main/java/com/pariaslab/callun/MoonPhases.kt
+++ app/src/main/java/com/pariaslab/callun/MoonPhases.kt
@@ -23,25 +23,14 @@ fun getMoonLapse(date: Calendar): Double {
 
 fun getMoonLapseIcon(date: Calendar): Int {
     val phase = getMoonLapse(date)
-    val phaseInt = (phase * 15.0).roundToInt()
-    val drawableId = when (phaseInt) {
-        0 -> R.drawable.moon0
-        1 -> R.drawable.moon1
-        2 -> R.drawable.moon2
-        3 -> R.drawable.moon3
-        4 -> R.drawable.moon4
-        5 -> R.drawable.moon5
-        6 -> R.drawable.moon6
-        7 -> R.drawable.moon7
-        8 -> R.drawable.moon8
-        9 -> R.drawable.moon9
-        10 -> R.drawable.moon10
-        11 -> R.drawable.moon11
-        12 -> R.drawable.moon12
-        13 -> R.drawable.moon13
-        14 -> R.drawable.moon14
-        15 -> R.drawable.moon15
-        else -> R.drawable.ic_launcher_foreground
+    val phaseInt = (phase * 55.0).roundToInt()
+    val resourceName = "frame_$phaseInt"
+    val packageName = "com.example.callun"
+    return try {
+        val field = Class.forName("$packageName.R\$drawable").getField(resourceName)
+        field.getInt(null)
+    } catch (e: Exception) {
+        e.printStackTrace()
+        R.drawable.ic_launcher_foreground
     }
-    return drawableId
 }
